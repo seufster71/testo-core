0a8e119121946c08ee96e15cdb84095f4627e4cd
edward.seufert <edward.seufert@TPA1WS01190.corp.autoclubfl.com>
2017-09-21 09:23:19 -0400

    init

---
+++ testo-core/src/main/java/org/testo/TestoCoreApplication.java
@@ -0,0 +1,12 @@
+package org.testo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestoCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestoCoreApplication.class, args);
+	}
+}
